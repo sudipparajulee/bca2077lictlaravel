abecc727bb6bb1d2a6e814b9b8dfc1bcbb30a8e7
Sudip Parajuli <sudip.parajulee@gmail.com>
2024-07-10 10:24:05 +0545

    Mail

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.master')
+@section('content')
+    <h2 class="text-5xl font-bold text-center my-10">404 Not Found</h2>
+@endsection
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.master')
+@section('content')
+    <h2 class="text-5xl font-bold text-center my-10">Sorry, the resource you are requesting is Not Found</h2>
+@endsection
